73d50404451568b565b2ba2e29b40a3e41e99769
Magido Mascate <magido@google.com>
2023-04-05 14:25:39 +0000

    Update repo from cloudtop

--- playbooks/modules_playbook.go
+++ playbooks/modules_playbook.go
@@ -1,5 +1,7 @@
 package playbooks
 
+import "google.golang.org/genproto/googleapis/devtools/containeranalysis/v1beta1/source"
+
 type TFImportModulesFactory interface{
 	/*Check  TF importing resource meta file EXISTS OR CREATE NEW.
     For instance, a TF importing resource meta file contains a reference of service folders as modules"
@@ -13,6 +15,38 @@ type TFImportModulesFactory interface{
     */
 	CheckTFMetaModulesFileExistsOrCreate () bool
 
-	/*
+	/*Construct a meta module for importing resource type. For instance. meta modules have source field
+    pointing to the resources relative path.
+    Example 1:
+	    /Projects/
+	    |--CloudLabs/       
+	    |  |--Compute/      
+	    |  |  |--Instance.tf
+         |--meta_modules.tf   # content example
+             [truncated]
+             module "compute" {
+                source = "./compute"
+             }
+             [truncated]
 	*/
+  ConstructTFMetaModule() string 
+}
+
+
+/*Also it can have a `resources block, optional` as:
+ resources = list(object({
+  resource_type = string  # e.g. google_compute_instance, google_compue_disk
+  identifier    = string  # e.g. workernode, workernode-disk
+  resource_id   = string  # e.g. projects/-/zones/-/instances/workernode
+ }))   
+*/
+type TFMetaModule struct {
+  source string
+  //resources []interface{}
+}
+
+
+func (t TFMetaModule) ConstructTFMetaModule() string  {
+  t.source = ""
+  return ""
 }
